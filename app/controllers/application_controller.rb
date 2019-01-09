7a05bf4366a3a091c710d7815a12d8e46a0d818c
Stefan Yakimov <stefan.yakimov@receipt-bank.com>
2019-01-09 17:30:25 +0200

    Refactor and extract User model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
   def authenticate
-    redirect_to new_session_path unless signed_in?
+    redirect_to new_session_path unless current_user.signed_in?
   end
 
-  def signed_in?
-    session[:current_email].present?
+  def sign_in(email)
+    session[:current_email] = email
+  end
+
+  def current_user
+    User.new(session[:current_email])
   end
 end
